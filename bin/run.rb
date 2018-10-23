097bd3fd543fd735be1db65dc1f0c525912f623c
imanj12 <imanj12@gmail.com>
2018-10-23 16:06:21 -0400

    changed run.rb cli input - check comments

--- bin/run.rb
+++ bin/run.rb
@@ -2,5 +2,5 @@ require_relative '../config/environment'
 
 cli = CommandLineInterface.new
 
-input = cli.greet
-cli.greet_input(input)
+# input = cli.greet
+cli.greet_input(cli.greet) # changed because cli.greet returns the input we need for cli.greet_input anyway
