37c7dafaa7e8da58487486503f89699bfe79974e
github username <github email address>
2018-10-24 14:00:49 -0400

    added find_by_artist in api_communicator.rb

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,23 +6,28 @@ class ApiCommunicator
     #json_string = RestClient.get('https://app.ticketmaster.com/discovery/v2/events?apikey=Qp2WTAvTGAHltXmm8Vc8ARAU5qvAx2nw&keyword=930%20Club&city=Washington&countryCode=US&stateCode=DC')
 
     #puts json_string
-
-    def self.get_parse(location,artist)
-        puts city = location.split(", ")[0].gsub(" ", "%20")
-    	puts state = location.split(", ")[1]
+     
+    def self.find_by_artist(location,artist)
+        city = location.split(", ")[0].gsub(" ", "%20")
+        state = location.split(", ")[1]
     	
      	artist.gsub!(" ", "%20")
 
-    	
+    	puts "im here too"
     	response_string = RestClient.get("https://app.ticketmaster.com/discovery/v2/events?apikey=Qp2WTAvTGAHltXmm8Vc8ARAU5qvAx2nw&keyword=#{artist}&city=#{city}&countryCode=US&stateCode=#{state}")
         response_hash = JSON.parse(response_string)
 
-        puts response_hash
+        new_artist = Artist.create(name: response_hash["_embedded"]["events"][0]["name"])
+        puts new_artist.name
+
+         
 
     end
+
+    
 
 
 
 end
 
-ApiCommunicator.get_parse("Washington, DC","Wild Nothing")
+
